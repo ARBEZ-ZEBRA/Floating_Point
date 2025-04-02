c1c57952305ca674028bf5ae3efc01bc8d9cbf20
ARBEZ-ZEBRA <Kenny56387866@163.com>
2025-04-02 17:19:54 +0800

    Add files via upload

--- FloatingPoint_1.hpp
+++ FloatingPoint_1.hpp
@@ -17,6 +17,7 @@ typedef enum STATE
     Nan
 } State;
 
+// 找到左数第一个一
 int findFirstOneBit(uint64_t bin_value)
 {
     if (bin_value == 0)
@@ -72,6 +73,109 @@ public:
     uint64_t get_E_mask() const { return E_mask; }
     uint64_t get_M_mask() const { return M_mask; }
 
+    // Print the state of the floating point number
+    void print_state() const
+    {
+        std::cout << "Decimal: " << To_decimal() << std::endl;
+        std::cout << "Sign: " << sign << "\n";
+        std::cout << "Exponent: 0b" << std::bitset<exponent>(E_value) << "\n";
+        std::cout << "Mantissa: 0b" << std::bitset<mantissa>(M_value) << "\n";
+        std::cout << "State: ";
+        switch (state)
+        {
+        case Normal:
+            std::cout << "Normal\n";
+            break;
+        case Subnormal:
+            std::cout << "Subnormal\n";
+            break;
+        case Zero:
+            std::cout << "Zero\n";
+            break;
+        case Inf:
+            std::cout << "Inf\n";
+            break;
+        case Nan:
+            std::cout << "NaN\n";
+            break;
+        }
+    }
+
+    State update_state(FloatingPoint value) const
+    {
+        if (value.E_value == 0 && value.M_value == 0)
+        {
+            return Zero;
+        }
+        else if (value.E_value == 0 && value.M_value != 0)
+        {
+            return Subnormal;
+        }
+        else if (value.E_value == value.E_mask && value.M_value == 0)
+        {
+            return Inf;
+        }
+        else if (value.E_value == value.E_mask && value.M_value != 0)
+        {
+            return Nan;
+        }
+        else
+        {
+            return Normal;
+        }
+    }
+
+    double To_decimal() const
+    {
+        uint64_t bias = (1ULL << (exponent - 1)) - 1;
+
+        uint64_t E = static_cast<uint64_t>(E_value & E_mask);
+        if (E == 0)
+        {
+            double base = 0;
+            E = 1 - bias;
+        }
+        else
+        {
+            E -= bias;
+        }
+
+        double M = M_value & M_mask;
+        if (E != 1 - bias)
+        {
+            M += (1ULL << mantissa);
+        }
+
+        double result = std::pow(2.0, E) * M / (1ULL << mantissa);
+
+        if (sign)
+        {
+            result = -result;
+        }
+
+        return result;
+    }
+
+    static FloatingPoint<exponent, mantissa> createInfinity(bool sign)
+    {
+        FloatingPoint<exponent, mantissa> result;
+        result.sign = sign;
+        result.E_value = (1ULL << exponent) - 1;
+        result.M_value = 0;
+        result.state = Inf;
+        return result;
+    }
+
+    static FloatingPoint<exponent, mantissa> createZero()
+    {
+        FloatingPoint<exponent, mantissa> result;
+        result.sign = 0;
+        result.E_value = 0;
+        result.M_value = 0;
+        result.state = Zero;
+        return result;
+    }
+
     // change the floatingpoint into binary form
     template <int other_exponent, int other_mantissa>
     uint64_t Bin(FloatingPoint<other_exponent, other_mantissa> value)
@@ -103,8 +207,10 @@ public:
     // Initialize with int value
     FloatingPoint(int value)
     {
-        int F1 = findFirstOneBit(value);
         sign = (value < 0);
+        if (value < 0)
+            value = -value;
+        int F1 = findFirstOneBit(value);
         E_value = F1 + (E_mask >> 1);
         M_value = (value & ((1ULL << F1) - 1)) << (mantissa - F1);
         update_state();
@@ -161,7 +267,7 @@ public:
     }
 
     template <int other_exponent, int other_mantissa>
-    FloatingPoint& operator=(const FloatingPoint<other_exponent, other_mantissa> &value)
+    FloatingPoint &operator=(const FloatingPoint<other_exponent, other_mantissa> &value)
     {
         uint64_t bin_value = Bin(value);
         // std::cout << std::bitset<64>(bin_value) << std::endl;
@@ -175,7 +281,7 @@ public:
     }
 
     template <int other_exponent, int other_mantissa>
-    FloatingPoint& operator=(const FloatingPoint<other_exponent, other_mantissa> &&value)
+    FloatingPoint &operator=(const FloatingPoint<other_exponent, other_mantissa> &&value)
     {
         uint64_t bin_value = Bin(value);
         // std::cout << std::bitset<64>(bin_value) << std::endl;
@@ -184,23 +290,27 @@ public:
         E_value = temp.get_E_value();
         M_value = temp.get_M_value();
         update_state();
-        
+
         return *this;
     }
 
-    FloatingPoint& operator=(int value) {
+    FloatingPoint &operator=(int value)
+    {
         return FloatingPoint(value);
     }
 
-    FloatingPoint& operator=(float value) {
+    FloatingPoint &operator=(float value)
+    {
         return FloatingPoint(value);
     }
 
-    FloatingPoint& operator=(double value) {
+    FloatingPoint &operator=(double value)
+    {
         return FloatingPoint(value);
     }
 
-    FloatingPoint& operator=(uint64_t value) {
+    FloatingPoint &operator=(uint64_t value)
+    {
         return FloatingPoint(value);
     }
 
@@ -443,54 +553,305 @@ public:
     }
 
     /* Unary Operation */
-    FloatingPoint neg() const {
+    FloatingPoint neg() const
+    {
         return FloatingPoint(!sign, E_value, M_value);
     }
 
-    FloatingPoint operator-() const {
+    FloatingPoint operator-() const
+    {
         return neg();
     }
 
-    friend FloatingPoint neg(const FloatingPoint& value) {
+    friend FloatingPoint neg(const FloatingPoint &value)
+    {
         return value.neg();
     }
 
-    FloatingPoint abs() const {
+    FloatingPoint abs() const
+    {
         return FloatingPoint(false, E_value, M_value);
     }
 
-    friend FloatingPoint abs(const FloatingPoint& value) {
+    friend FloatingPoint abs(const FloatingPoint &value)
+    {
         return value.abs();
     }
 
-    // Print the state of the floating point number
-    void print_state() const
+    FloatingPoint sqrt() const
     {
-        std::cout << "Sign: " << sign << "\n";
-        std::cout << "Exponent: 0b" << std::bitset<exponent>(E_value) << "\n";
-        std::cout << "Mantissa: 0b" << std::bitset<mantissa>(M_value) << "\n";
-        std::cout << "State: ";
-        switch (state)
+        if (sign == true)
         {
-        case Normal:
-            std::cout << "Normal\n";
-            break;
-        case Subnormal:
-            std::cout << "Subnormal\n";
-            break;
-        case Zero:
-            std::cout << "Zero\n";
-            break;
-        case Inf:
-            std::cout << "Inf\n";
-            break;
-        case Nan:
-            std::cout << "NaN\n";
-            break;
+            std::cout << "Negative Input!!!" << std::endl;
+        }
+        else
+        {
         }
     }
 
-    
+    // Addition
+    FloatingPoint add(const FloatingPoint &other) const
+    {
+        if (state == Nan)
+        {
+            return *this;
+        }
+        else if (other.state == Nan)
+        {
+            return other;
+        }
+
+        if (state == Inf)
+        {
+            if (other.state == Inf)
+            {
+                return (sign == other.sign) ? *this : createZero();
+            }
+            return *this;
+        }
+
+        if (other.state == Inf)
+        {
+            return other;
+        }
+
+        if (state == Zero)
+        {
+            return other;
+        }
+
+        if (other.state == Zero)
+        {
+            return *this;
+        }
+
+        // both normal case
+        bool sign1 = sign;
+        int64_t exp1 = static_cast<int64_t>(E_value) - ((1ULL << (exponent - 1)) - 1);
+        uint64_t mantissa1 = (state == Subnormal) ? M_value : M_value | (1ULL << mantissa);
+
+        bool sign2 = other.sign;
+        int64_t exp2 = static_cast<int64_t>(other.E_value) - ((1ULL << (exponent - 1)) - 1);
+        uint64_t mantissa2 = (other.state == Subnormal) ? other.M_value : other.M_value | (1ULL << mantissa);
+
+        // Align exponents by shifting the smaller number's mantissa right
+        int64_t exp_diff = exp1 - exp2;
+        if (exp_diff < 0)
+        {
+            std::swap(sign1, sign2);
+            std::swap(exp1, exp2);
+            std::swap(mantissa1, mantissa2);
+            exp_diff = -exp_diff;
+        }
+
+        if (exp_diff > static_cast<int64_t>(mantissa))
+        {
+            // The smaller number is too small to affect the larger one
+            return *this;
+        }
+
+        mantissa1 <<= exp_diff;
+
+        // Perform addition or subtraction based on signs
+        uint64_t result_mantissa;
+        bool result_sign;
+        if (sign1 == sign2)
+        {
+            result_sign = sign1;
+            result_mantissa = mantissa1 + mantissa2;
+        }
+        else
+        {
+            if (mantissa1 > mantissa2)
+            {
+                result_mantissa = mantissa1 - mantissa2;
+                result_sign = sign1;
+            }
+            else
+            {
+                result_mantissa = mantissa2 - mantissa1;
+                result_sign = sign2;
+            }
+        }
+
+        // Normalize the result
+        if (result_mantissa == 0)
+        {
+            return createZero();
+        }
+
+        if ((result_mantissa & ((1ULL << exp_diff) - 1)) > 0)
+        {
+            result_mantissa >>= exp_diff;
+            result_mantissa += 1;
+        }
+        else
+        {
+            result_mantissa >>= exp_diff;
+        }
+
+        int first = findFirstOneBit(result_mantissa);
+        result_mantissa &= ((1ULL << first) - 1);
+
+        if (first >= mantissa)
+        {
+            if ((result_mantissa & ((1ULL << (first - mantissa)) - 1)) > 0)
+            {
+                result_mantissa >>= (first - mantissa);
+                result_mantissa += 1;
+            }
+            else
+            {
+                result_mantissa >>= (first - mantissa);
+            }
+            exp1 += (first - mantissa);
+        }
+        else
+        {
+            result_mantissa <<= (mantissa - first);
+            exp1 -= (mantissa - first);
+        }
+
+        // Pack the result
+        FloatingPoint<exponent, mantissa> result;
+        result.sign = result_sign;
+        result.E_value = static_cast<uint64_t>(exp1 + ((1ULL << (exponent - 1)) - 1));
+        result.M_value = result_mantissa & ((1ULL << mantissa) - 1);
+        result.state = update_state(result);
+
+        return result;
+    }
+    friend FloatingPoint add(const FloatingPoint &fp1, const FloatingPoint &fp2)
+    {
+        return fp1.add(fp2);
+    }
+    FloatingPoint operator+(const FloatingPoint &other) const
+    {
+        return add(other);
+    };
+    friend FloatingPoint operator+(int val, const FloatingPoint &fp)
+    {
+        return FloatingPoint(val) + fp;
+    }
+    friend FloatingPoint operator+(const FloatingPoint &fp, int val)
+    {
+        return fp + FloatingPoint(val);
+    }
+    friend FloatingPoint operator+(double val, const FloatingPoint &fp)
+    {
+        return FloatingPoint(val) + fp;
+    }
+    friend FloatingPoint operator+(const FloatingPoint &fp, double val)
+    {
+        return fp + FloatingPoint(val);
+    }
+    FloatingPoint operator+=(const FloatingPoint &other)
+    {
+        *this = add(other);
+        return *this;
+    }
+    FloatingPoint operator+=(int val)
+    {
+        *this = add(FloatingPoint(val));
+        return *this;
+    }
+    FloatingPoint operator+=(double val)
+    {
+        *this = add(FloatingPoint(val));
+        return *this;
+    }
+
+    // Subtraction
+    FloatingPoint sub(const FloatingPoint &other) const
+    {
+        return add(other.neg());
+    }
+    friend FloatingPoint sub(const FloatingPoint &fp1, const FloatingPoint &fp2)
+    {
+        return fp1.sub(fp2);
+    }
+    FloatingPoint operator-(const FloatingPoint &other) const
+    {
+        return sub(other);
+    };
+    friend FloatingPoint operator-(int val, const FloatingPoint &fp)
+    {
+        return FloatingPoint(val) - fp;
+    }
+    friend FloatingPoint operator-(const FloatingPoint &fp, int val)
+    {
+        return fp - FloatingPoint(val);
+    }
+    friend FloatingPoint operator-(double val, const FloatingPoint &fp)
+    {
+        return FloatingPoint(val) - fp;
+    }
+    friend FloatingPoint operator-(const FloatingPoint &fp, double val)
+    {
+        return fp - FloatingPoint(val);
+    }
+    FloatingPoint operator-=(const FloatingPoint &other)
+    {
+        *this = sub(other);
+        return *this;
+    }
+    FloatingPoint operator-=(int val)
+    {
+        *this = sub(FloatingPoint(val));
+        return *this;
+    }
+    FloatingPoint operator-=(double val)
+    {
+        *this = sub(FloatingPoint(val));
+        return *this;
+    }
+
+    // Comparison Operator
+    bool operator==(const FloatingPoint &other) const
+    {
+        if ((state == Nan) || (other.state == Nan))
+        {
+            std::cout << "The input of compare function can not be NAN !!!" << std::endl;
+            return 0;
+        }
+        return sign == other.sign && E_value == other.E_value && M_value == other.M_value;
+    };
+    bool operator!=(const FloatingPoint &other) const
+    {
+        return !(*this == other);
+    };
+    bool operator<(const FloatingPoint &other) const
+    {
+        if ((state == Nan) || (other.state == Nan))
+        {
+            std::cout << "The input of compare function can not be NAN !!!" << std::endl;
+            return 0;
+        }
+        if (sign != other.sign)
+        {
+            return sign;
+        }
+        else if (E_value != other.E_value)
+        {
+            return sign ? E_value > other.E_value : E_value < other.E_value;
+        }
+        else
+        {
+            return sign ? M_value > other.M_value : M_value < other.M_value;
+        }
+    };
+    bool operator<=(const FloatingPoint &other) const
+    {
+        return *this < other || *this == other;
+    };
+    bool operator>(const FloatingPoint &other) const
+    {
+        return !(*this <= other);
+    };
+    bool operator>=(const FloatingPoint &other) const
+    {
+        return !(*this < other);
+    };
 };
 
 #endif // FLOATINGPOINT_HPP_
